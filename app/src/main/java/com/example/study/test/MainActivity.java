5dd5972438d49bb6f94253c618197c2a53fcdec2
xiangyang.li <562315079@qq.com>
2016-06-20 23:17:06 +0800

    right image orientation, autofocus only first time, wrong image resolution

--- app/src/main/java/com/example/study/test/MainActivity.java
+++ app/src/main/java/com/example/study/test/MainActivity.java
@@ -1,84 +1,68 @@
 package com.example.study.test;
 
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.Date;
-
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+
+import android.graphics.Bitmap;
 import android.hardware.Camera;
+import android.hardware.Camera.PictureCallback;
+import android.hardware.Camera.ShutterCallback;
+
+import android.os.BatteryManager;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import android.view.*;
 import android.hardware.Camera.AutoFocusCallback;
 import android.hardware.Camera.PictureCallback;
-import android.os.AsyncTask;
-import android.os.Build;
-import android.os.Bundle;
-import android.os.Environment;
-import android.text.format.DateFormat;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceHolder.Callback;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.graphics.Bitmap;
-import android.graphics.Bitmap.CompressFormat;
-import android.graphics.BitmapFactory;
-import android.graphics.ImageFormat;
-import android.hardware.Camera;
-import android.hardware.Camera.AutoFocusCallback;
-import android.hardware.Camera.PictureCallback;
-import android.hardware.Camera.ShutterCallback;
-import android.os.Bundle;
-import android.os.Environment;
+import java.util.ArrayList;
+import java.util.Set;
+
+import android.widget.Toast;
+
 import android.util.DisplayMetrics;
 import android.view.Display;
-import android.view.Menu;
-import android.view.SurfaceHolder;
-import android.view.SurfaceHolder.Callback;
-import android.view.LayoutInflater;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.EditText;
-import android.widget.ImageView;
 
-public class MainActivity extends Activity {
-
-    SurfaceView sView;
-    SurfaceHolder surfaceHodler;
-    int screenWidth, screenHeight;
-    // 定义系统所用的照相机
+public class MainActivity extends Activity implements SurfaceHolder.Callback {
     Camera camera;
-    // 是否存在预览中
-    boolean isPreview = false;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
 
+    Camera.PictureCallback rawCallback;
+    Camera.ShutterCallback shutterCallback;
+    Camera.PictureCallback jpegCallback;
+
+    int screenWidth, screenHeight;
+    AutoFocusCallback autoFocusCallback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // 设置全屏
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+
+        //set full screen
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
-        // 获取窗口管理器
+        //get the window manager
         WindowManager wm = getWindowManager();
         Display display = wm.getDefaultDisplay();
         DisplayMetrics metrics = new DisplayMetrics();
@@ -86,183 +70,146 @@ public class MainActivity extends Activity {
         display.getMetrics(metrics);
         screenWidth = metrics.widthPixels;
         screenHeight = metrics.heightPixels;
-        sView = (SurfaceView) findViewById(R.id.sView);
-        // 设置surface不需要自己的维护缓存区
-        sView.getHolder().setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-        // 获得SurfaceView的SurfaceHolder
-        surfaceHodler = sView.getHolder();
-        // 为srfaceHolder添加一个回调监听器
-        surfaceHodler.addCallback(new Callback() {
 
+        surfaceView = (SurfaceView) findViewById(R.id.surfaceView);
+        surfaceView.setZOrderOnTop(true);
+
+        surfaceHolder = surfaceView.getHolder();
+        // 为srfaceHolder添加一个回调监听器
+        surfaceHolder.addCallback(this);
+        // 设置surface不需要自己的维护缓存区
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        jpegCallback = new PictureCallback() {
             @Override
-            public void surfaceDestroyed(SurfaceHolder arg0) {
-                // 如果camera不为null，释放摄像头
-                if (camera != null) {
-                    if (isPreview)
-                        camera.stopPreview();
-                    camera.release();
-                    camera = null;
+            public void onPictureTaken(byte[] data, Camera camera) {
+                FileOutputStream outStream = null;
+                try {
+                    outStream = new FileOutputStream(String.format("/sdcard/%d.jpg", System.currentTimeMillis()));
+                    outStream.write(data);
+                    outStream.close();
+                }
+                catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                }
+                Toast.makeText(getApplicationContext(), "Picture Saved", Toast.LENGTH_LONG).show();
+                refreshCamera();
+            }
+        };
+        autoFocusCallback = new Camera.AutoFocusCallback() {
+            public void onAutoFocus(boolean success, Camera camera) {
+                // TODO Auto-generated method stub
+                if(success){
+                    camera.setOneShotPreviewCallback(null);
+                    Toast.makeText(getApplicationContext(),"autofocus", Toast.LENGTH_SHORT).show();
                 }
             }
+        };
 
-            @Override
-            public void surfaceCreated(SurfaceHolder arg0) {
-                // 打开摄像头
-                initCamera();
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder arg0, int arg1, int arg2, int arg3) {
-            }
-        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void captureImage(View v) throws IOException {
+        camera.takePicture(null, null, jpegCallback);
     }
 
-    private void initCamera() {
-        if (!isPreview) {
-            // 此处默认打开后置摄像头
-            // 通过传入参数可以打开前置摄像头
-            camera = Camera.open();
-            camera.setDisplayOrientation(90);
+    public void refreshCamera() {
+        if (surfaceHolder.getSurface() == null) {
+            return;
         }
-        if (!isPreview && camera != null) {
-            Camera.Parameters parameters = camera.getParameters();
-            // 设置预览照片的大小
-            parameters.setPreviewSize(screenWidth, screenHeight);
-            // 设置预览照片时每秒显示多少帧的最小值和最大值
-            parameters.setPreviewFpsRange(4, 10);
-            // 设置照片的格式
-            parameters.setPictureFormat(ImageFormat.JPEG);
-            // 设置JPG照片的质量
-            parameters.set("jpeg-quality", 85);
-            // 设置照片的大小
-            parameters.setPictureSize(screenWidth, screenHeight);
-            // 通过SurfaceView显示取景画面
-            try {
-                camera.setPreviewDisplay(surfaceHodler);
-            } catch (IOException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-            // 开始预览
+
+        try {
+            camera.stopPreview();
+        }
+
+        catch (Exception e) {
+        }
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
             camera.startPreview();
-            isPreview = true;
         }
-    }
-
-    public void capture(View source) {
+        catch (Exception e) {
+        }
         if (camera != null) {
             // 控制摄像头自动对焦后才拍摄
             camera.autoFocus(autoFocusCallback);
         }
     }
 
-    AutoFocusCallback autoFocusCallback = new AutoFocusCallback() {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 
-        @Override
-        public void onAutoFocus(boolean arg0, Camera arg1) {
-            if (arg0) {
-                // takePicture()方法需要传入三个监听参数
-                // 第一个监听器；当用户按下快门时激发该监听器
-                // 第二个监听器；当相机获取原始照片时激发该监听器
-                // 第三个监听器；当相机获取JPG照片时激发该监听器
-                camera.takePicture(new ShutterCallback() {
-                    @Override
-                    public void onShutter() {
-                        // 按下快门瞬间会执行此处代码
-                    }
-                }, new PictureCallback() {
-                    @Override
-                    public void onPictureTaken(byte[] arg0, Camera arg1) {
-                        // 此处代码可以决定是否需要保存原始照片信息
-                    }
-                }, myJpegCallback);
-            }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
         }
-    };
-    PictureCallback myJpegCallback = new PictureCallback() {
+        return super.onOptionsItemSelected(item);
+    }
 
-        @Override
-        public void onPictureTaken(byte[] data, Camera camera) {
-            /*
-            // 根据拍照所得的数据创建位图
-            final Bitmap bm = BitmapFactory.decodeByteArray(data, 0,
-                    data.length);
-            // 加载布局文件
-            View saveDialog = getLayoutInflater().inflate(R.layout.save, null);
-            final EditText potoName = (EditText) saveDialog
-                    .findViewById(R.id.photoNmae);
-            // 获取saveDialog对话框上的ImageView组件
-            ImageView show = (ImageView) saveDialog.findViewById(R.id.show);
-            // 显示刚刚拍得的照片
-            show.setImageBitmap(bm);
-            // 使用AlertDialog组件
-            new AlertDialog.Builder(MainActivity.this)
-                    .setView(saveDialog)
-                    .setNegativeButton("取消", null)
-                    .setPositiveButton("保存",
-                            new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface arg0,
-                                                    int arg1) {
-                                    // 创建一个位于SD卡上的文件
-                                    File file = new File(Environment
-                                            .getExternalStorageDirectory()
-                                            + "/"
-                                            + potoName.getText().toString()
-                                            + ".jpg");
-                                    FileOutputStream  fileOutStream=null;
-                                    try {
-                                        fileOutStream=new FileOutputStream(file);
-                                        //把位图输出到指定的文件中
-                                        bm.compress(CompressFormat.JPEG, 100, fileOutStream);
-                                        fileOutStream.close();
-                                    } catch (IOException io) {
-                                        io.printStackTrace();
-                                    }
-
-                                }
-                            }).show();
-            //重新浏览
-            camera.stopPreview();
-            camera.startPreview();
-            isPreview=true;
-            */
-
-            new SavePictureTask().execute(data);
-            camera.stopPreview();
-            camera.startPreview();
-            isPreview = true;
-
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera = Camera.open();
+            camera.setDisplayOrientation(90);
         }
-    };
 
-    /**
-     * 使用异步任务 进行文件操作
-     *
-     * @author Administrator
-     */
-    class SavePictureTask extends AsyncTask<byte[], String, String> {
+        catch (RuntimeException e) {
+            System.err.println(e);
+            return;
+        }
 
-        protected String doInBackground(byte[]... params) {
-            String fname = DateFormat.format("yyyyMMddhhmmss", new Date()).toString() + ".jpg";
-            File jpgFile = new File(Environment.getExternalStorageDirectory() + "/" + fname);
-            try {
-                FileOutputStream fos = new FileOutputStream(jpgFile.getPath());
-                fos.write(params[0]);
-                fos.close();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            return null;
+        Camera.Parameters param;
+        param = camera.getParameters();
+        param.setPreviewSize(600, 800);
+        //rotate 180
+        param.setRotation(180);
+
+
+        camera.setParameters(param);
+
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        }
+
+        catch (Exception e) {
+            System.err.println(e);
+            return;
         }
     }
 
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        refreshCamera();
+    }
 
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
+    }
 }
